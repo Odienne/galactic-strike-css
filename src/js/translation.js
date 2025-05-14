04199b7945960d7bcd158da02b816e84e7c1ab7c
Adam Odienne <odienne.adam@gmail.com>
2025-05-14 14:25:26 +0200

    update lang is no longer given in url

--- src/js/translation.js
+++ src/js/translation.js
@@ -1,6 +1,12 @@
-const getLangParam = () =>  {
-    const urlParams = new URLSearchParams(window.location.search);
-    return urlParams.get('lang') || 'fr'; // default to French if no lang param is provided
+let lang = 'fr';
+let translations = {};
+
+const setLanguage = (newLang) => {
+    lang = newLang;
+
+    translateAll();
+
+    return lang;
 }
 
 const getTranslations = async (lang) => {
@@ -18,12 +24,17 @@ const translate = (key) => {
     return translations[key] || key; // return the original key if no translation is found
 }
 
-const lang = getLangParam();
-let translations = {};
-
-window.addEventListener("DOMContentLoaded", async () => {
+async function translateAll() {
     translations = await getTranslations(lang);
 
     // Now safely call translate and update the DOM
     document.getElementById("score-text").textContent = translate("score");
+}
+
+
+window.addEventListener("DOMContentLoaded", () => {
+    setLanguage('fr');
 });
+
+//to make it accessible to Qt app
+window.setLanguage = setLanguage;
