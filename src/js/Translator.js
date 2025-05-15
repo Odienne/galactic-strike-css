6b5e0068746dbf3d08ea29744eebabedefa6096d
Adam Odienne <odienne.adam@gmail.com>
2025-05-15 12:24:37 +0200

    update sounds

--- src/js/Translator.js
+++ src/js/Translator.js
@@ -3,8 +3,6 @@ import en from '../data/translations/translations.en.json';
 
 export default class Translator {
     constructor(lang) {
-        this.lang = lang;
-        this.translations = lang === 'fr' ? fr : en;
+        this.translations = lang.toLowerCase() === 'fr' ? fr : en;
     }
-
 }
